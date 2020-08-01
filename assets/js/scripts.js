ca7f83aac620ce99b443957d5d2f2a0a53c92122
Daniel_mor <danielmoragarodriguez@gmail.com>
2020-08-01 08:39:09 -0400

    quinto envio

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,28 @@
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
+$(function(){
+  $("a").click(function(event) {
+
+  if (this.hash !== "") {
+  event.preventDefault();
+
+  var gato = this.hash;
+
+  $('html, body').animate({
+  scrollTop: $(gato).offset().top}, 800, function(){
+
+  window.location.hash = gato;
+  });
+  } // Fin del if
+  });
+  });  
+  
+  
+  
+  //ventanas modales
+  $('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
   })
+
+    //tooltip
+    $(function () {
+      $('[data-toggle="tooltip"]').tooltip()
+    })
